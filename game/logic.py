6c9e5db70d7919410624388eb22054fcb1bfe8f9
Moritz Nisblé <moritz.nisble@gmx.de>
2015-10-30 15:21:27 +0100

    Added matrix generator and print methods. (python rocks :)

--- game/logic.py
+++ game/logic.py
@@ -14,26 +14,41 @@ class Field(object):
         self.row = row
         self.bomb = bomb
 
+    def print_mark(self):
+        return 'X' if self.bomb else '0'
+
     def __str__(self):
         return 'C:{} R:{} B:{}'.format(self.column, self.row, self.bomb)
 
 
-def column_generator(rows):
-    pass
+class Matrix(object):
+    def _column_generator(self, column, rows):
+        return [Field(column, r) for r in range(rows)]
 
+    def _matrix_generator(self, columns=2, rows=2):
+        return [self._column_generator(c, rows) for c in range(columns)]
 
-def matrix_generator(columns=2, rows=2, bombs=1):
-    matrix = []
-    for c in range(columns):
-        matrix.append([])
-        for r in range(rows):
-            matrix[c].append(Field(c, r))
-    return matrix
+    def __init__(self, columns=2, rows=2, bombs=1):
+        self.matrix = self._matrix_generator(columns, rows)
+
+    def _str(self, getter):
+        string = ''
+        for r in range(len(self.matrix[0])):
+            for c in range(len(self.matrix)):
+                string += getter(c, r)
+                string += ' '
+            string += '\n'
+        return string
+
+    def str_matrix(self):
+        return self._str(lambda c, r: self.matrix[c][r].print_mark())
+
+    def __str__(self):
+        return self._str(lambda c, r: str(self.matrix[c][r]))
 
 
 if __name__ == '__main__':
     logging.info("Run tests")
-    matrix = matrix_generator()
-    for c in range(len(matrix)):
-        for r in range(len(matrix[c])):
-            print(matrix[c][r])
+    matrix = Matrix(10, 10)
+    print('__str__:\n{}'.format(matrix))
+    print('str_matrix:\n{}'.format(matrix.str_matrix()))
